5e10522fe58a9b326f3cf46bad583d67742741d2
Alessio Nossa <alessio.nossa@gmail.com>
2022-06-02 18:36:23 +0200

    Added sample project

---
+++ swift-ndi Sample/swift-ndi Sample/swift_ndi_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ndi_SampleApp.swift
+//  swift-ndi Sample
+//
+//  Created by Alessio Nossa on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_ndi_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
